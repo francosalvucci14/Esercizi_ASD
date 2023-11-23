1d3bdd3af1b6707a3caf697dff761074d7715945
francosalvucci14 <francosalvucci14@gmail.com>
2023-11-23 15:25:29 +0100

    Update

--- Algoritmi/algoritmoDFS.py
+++ Algoritmi/algoritmoDFS.py
@@ -4,6 +4,7 @@ class TreeNode:
         self.left = left
         self.right = right
 
+
 def DFS(root):
     S = []
     S.append(root)
@@ -13,28 +14,34 @@ def DFS(root):
             print(u.val)
             S.append(u.right)
             S.append(u.left)
+
+
 def DFS_postorder(root):
     if root != None:
         DFS_postorder(root.left)
         DFS_postorder(root.right)
         print(root.val)
+
+
 def DFS_sim(root):
     if root != None:
         DFS_sim(root.left)
         print(root.val)
         DFS_sim(root.right)
-        
+
+
 def DFS_preorder(root):
     if root != None:
         print(root.val)
         DFS_preorder(root.left)
         DFS_preorder(root.right)
 
+
 root = TreeNode("G")
 h = TreeNode("H")
-e=TreeNode("E")
+e = TreeNode("E")
 c = TreeNode("C")
-d=TreeNode("D")
+d = TreeNode("D")
 a = TreeNode("A")
 f = TreeNode("F")
 b = TreeNode("B")
@@ -47,7 +54,7 @@ d.left = a
 a.left = f
 a.right = b
 
-#DFS(root)
+# DFS(root)
 print("Visita in postordine")
 DFS_postorder(root)
 print("Visita simmetrica")
@@ -55,4 +62,4 @@ DFS_sim(root)
 print("Visita in preordine")
 DFS_preorder(root)
 
-#DFS(a)
+# DFS(a)
