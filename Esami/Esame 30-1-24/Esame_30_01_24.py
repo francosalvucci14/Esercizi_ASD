9e86f40ec31448d5e61cbcb6beefbee6b5151849
francosalvucci14 <francosalvucci14@gmail.com>
2024-02-02 15:32:00 +0100

    Update

--- Esami/Esame 30-1-24/Esame_30_01_24.py
+++ Esami/Esame 30-1-24/Esame_30_01_24.py
@@ -9,25 +9,27 @@ def build_structure(A):
 
     return prefisso_uni, prefisso_zeri
 
-def query(prefisso_uni, prefisso_zeri, i, j,n):
+
+def query(prefisso_uni, prefisso_zeri, i, j, n):
     if i == 0:
-        i=1
+        i = 1
     if j > n:
-        j=n
+        j = n
 
     num_uni = prefisso_uni[j] - prefisso_uni[i - 1]
     num_zeri = prefisso_zeri[j] - prefisso_zeri[i - 1]
 
-    if num_uni> num_zeri:
+    if num_uni > num_zeri:
         differenza_modulo = num_uni - num_zeri
     else:
         differenza_modulo = num_zeri - num_uni
 
     return differenza_modulo
 
+
 # Esempio di utilizzo
 vettore_A = [1, 0, 1, 1, 0, 1, 0, 0, 1]
 n = len(vettore_A)
 prefisso_uni, prefisso_zeri = build_structure(vettore_A)
-risultato_query = query(prefisso_uni, prefisso_zeri, 0, n,n)
+risultato_query = query(prefisso_uni, prefisso_zeri, 0, n, n)
 print(risultato_query)
